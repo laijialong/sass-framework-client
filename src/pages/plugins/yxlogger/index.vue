9f647fd2c36d6686a17d651ae65198159f241a8b
nick-zheng <mac_nic@163.com>
2019-06-03 20:52:29 +0800

    Update index.vue

--- src/pages/plugins/yxlogger/index.vue
+++ src/pages/plugins/yxlogger/index.vue
@@ -8,10 +8,18 @@
 </template>
 
 <script>
+  import dayjs from 'dayjs'
+
   export default {
     name:"index",
 
     onLoad(options){
+
+      console.log(dayjs("2019-06-03 13:49:57").add(500,'minute').format("YYYY-MM-DD HH:mm:ss"))
+
+      var flag = dayjs().isBefore(dayjs("2019-06-03 13:49:57").add(5,'minute'))
+      console.log(flag)
+
       this.$log.setLevel("debug")
       this.$log.setOutputWay(this.$log.OUTPUT_WAY_CONSOLE)
       this.$log.setMaxLogCacheCount(4)
